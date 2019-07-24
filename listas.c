b0e28436abfa8eb272b1236a609f024ebc3d8863
Maryanne Cerqueira <53245055+MaryanneCerqueira@users.noreply.github.com>
2019-07-24 00:01:21 -0300

    Delete listas.c

--- listas.c
+++
@@ -1,4 +0,0 @@
-//Este arquivo conterá as funções que lidam com listas, as quais serão utilizadas para elaborar grafos com listas de adjacência
-#include <stdio.h>
-#include <stdlib.h>
-#include "listas.h"
